48901f1345145434e83d2e93ee3e5f8657ececd5
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-05-10 14:49:55 -0500

    Make a get request to '/builds/:id'

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -3,4 +3,9 @@ class BuildsController < ApplicationController
     @builds = Build.all
     erb :'builds/index'
   end
+
+  get '/builds/:id' do
+    @build = Build.find(params[:id])
+    erb :'builds/show'
+  end
 end
