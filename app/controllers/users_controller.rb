da1daded5463fa81469c604373ca0f7f8b854f6f
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-05-11 16:42:07 -0500

    Validates username, email and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
       erb :'/users/register'
     end
 
+    post '/register' do
+      user = User.new(params)
+      if user.username.empty? || user.password.empty?
+        @error = "Username and password can't be blank"
+        erb :'users/register'
+      elsif User.find_by(username: user.username)
+        @error = "Account with that username already exists"
+        erb :'users/register'
+      else
+        user.save
+        session[:user_id] = user.id
+        redirect '/builds'
+      end
+
+    end
+
 end
