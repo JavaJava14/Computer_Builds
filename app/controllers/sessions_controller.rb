df6eeef0262d6604b175a7f00b05799a7610e9a1
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-05-11 17:44:03 -0500

    Make a post request to '/login'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,20 @@ class SessionsController < ApplicationController
       erb :'users/login'
     end
 
-    
+    post '/login' do 
+      if params["username"].empty? || params["password"].empty?
+        @error = "Username and password can't be blank"
+        erb :'users/login'
+      else
+        if user = User.find_by(username: params["username"], password: params["password"])
+        session[:user_id] = user.id
+        redirect '/builds'
+        else
+        @error = "Account not found"
+        erb :'users/login'
+        end
+      end
+
+    end
 
 end
