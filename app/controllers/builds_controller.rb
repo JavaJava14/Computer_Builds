52685338dc73736a33c9f886094d7d9be084c39a
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-05-10 20:34:13 -0500

    Make a post request to '/builds'

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,7 +1,18 @@
 class BuildsController < ApplicationController
 
   get '/builds/new' do
-    erb :'/builds/new'
+    erb :'builds/new'
+  end
+
+  post '/builds' do
+    build = Build.new(params)
+    if params[:title] != ""
+      build.save
+      redirect '/builds'
+    else
+      @error = "Please submit title and specs."
+      erb :'builds/new'
+    end
   end
 
   get '/builds' do
@@ -13,4 +24,5 @@ class BuildsController < ApplicationController
     @build = Build.find(params[:id])
     erb :'builds/show'
   end
+
 end
